95f4b7b60457817d1adbebe05bd3664732b8fc02
rodrigosamaniego <rodrigosamaniegopiza@gmail.com>
2021-11-24 11:16:15 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// ./index.js
+
+// 1. IMPORTACIONES
+const express		= require("express")
+const app			= express()
+
+const hbs			= require("hbs")
+
+const connectDB		= require("./config/db")
+
+require("dotenv").config()
+
+
+// 2. MIDDLEWARES
+app.use(express.static("public"))
+
+app.set("views", __dirname + "views")
+app.set("view engine", "hbs")
+
+hbs.registerPartials(__dirname + "/views/partials")
+
+app.use(express.urlencoded({ extended: true }))
+
+
+connectDB()
+
+// 3. RUTAS
+app.use("/users", require("./routes/users"))
+app.use("/", require("./routes/index"))
+
+// 4. SERVIDOR
+app.listen(process.env.PORT, () => {
+	console.log(`Corriendo en el puerto: ${process.env.PORT}`)
+})
